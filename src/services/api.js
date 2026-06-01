0d4c22648d07b5b3baeb023224b085a1eff3dff1
fops4 <156802707+fops4@users.noreply.github.com>
2026-06-01 15:12:27 +0100

    Update API_URL to new ngrok endpoint

--- src/services/api.js
+++ src/services/api.js
@@ -1,4 +1,4 @@
-const API_URL = import.meta.env.VITE_API_URL || "https://85bd-41-211-125-169.ngrok-free.app/api";
+const API_URL = import.meta.env.VITE_API_URL || "https://d477-41-211-125-169.ngrok-free.app/api";
 
 export const api = {
   async login(username, password) {
